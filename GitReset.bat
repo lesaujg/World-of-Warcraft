707685dad9ffe977071551842c531522f82d090b
lesaujg <lesaujg@gmail.com>
2017-01-09 12:04:09 -0500

    @home

---
+++ GitReset.bat
@@ -0,0 +1,3 @@
+git config remote.origin.url https://lesaujg:7ru57n0!@github.com/lesaujg/World-of-Warcraft.git
+git stash
+git stash drop
---
+++ GitUpdate.bat
@@ -0,0 +1,4 @@
+git config remote.origin.url https://lesaujg:7ru57n0!@github.com/lesaujg/World-of-Warcraft.git
+git add -A .
+git commit -m "@home"
+git push
