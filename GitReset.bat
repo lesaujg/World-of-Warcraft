e84c1d50bd0033b30034dd5cf74321b5d87c75b1
lesaujg <lesaujg@gmail.com>
2017-01-16 23:39:41 -0500

    @home

--- GitReset.bat
+++ GitReset.bat
@@ -1,5 +1,5 @@
 git config remote.origin.url https://lesaujg:7ru57n0!@github.com/lesaujg/World-of-Warcraft.git
 git add -A .
 git stash
-git stash drop stash@{0}
+git stash drop stash
 git pull <remote>
