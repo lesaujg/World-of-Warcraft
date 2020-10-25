a08a498e5800d187a4c74cac023d0e403f4a423c
lesaujg <lesaujg@gmail.com>
2020-10-25 09:22:36 -0400

    @home

--- GitUpdate.bat
+++ GitUpdate.bat
@@ -1,4 +1,4 @@
-git config remote.origin.url https://lesaujg:P3nn51cW@r@github.com/lesaujg/World-of-Warcraft.git
+git config remote.origin.url https://lesaujg@github.com/lesaujg/World-of-Warcraft.git
 git add -A .
 git commit -m "@home"
 git push
